fixed run scaling

AnalyzeData.py
@@ -7,6 +7,7 @@ import Visualize as Vis
 import Base as Base
 from Base import Constants as const
 import os
+import numpy as np
 
 def fileLoad():
         root = tk.Tk()
@@ -101,8 +102,8 @@ def plotSet():
     Vis.plotTemperatureLoaded(data, params, fileSet, folderSet, const=const())
     if len(data) < 2:
         Vis.plotTemperatureTimeLoaded(data, params, fileSet, folderSet)
-    plotSet = [0, int(params[0]["nSteps"])//4, params[0]["nSteps"]//2, params[0]["nSteps"]-1]
-
+    # plotSet = [0, int(params[0]["nSteps"])//4, params[0]["nSteps"]//2, params[0]["nSteps"]-1]
+    plotSet = np.floor(np.geomspace(1, params[0]["nSteps"]-1, 4)).astype(int)
     Vis.plot_spectra_at_times(data, plotSet, params, fileSet, folderSet, 20, params[0]["maxFreq"])
     Vis.analyzeRank(data, data[0]["timeSet"], params, fileSet, folderSet, time_indices=None, energy_threshold=[.999, .99, .98 ,.95], tol=None, plot=True)
     # Vis.analyzeRank(data["fullTensorPhi"], data["timeSet"], energy_threshold=None, tol = [1e-8, 1e-14, 1e-16, 1e-18], plot = True)

Visualize.py
@@ -387,7 +387,9 @@ def plot_spectra_at_times(dataSet, time_indices, paramsSet, fileSet, folderSet, 
         freqBase = 0.5 * (frequencies[:-1] + frequencies[1:])  # Midpoint of frequency groups for plotting
         if folder == "InfiniteMedium":
             methodName = "SM"
+            style = "-"
         else:
+            style = "--"
             methodName = "VCM"
         phi_tensor = data["fullTensorPhi"]
         n_steps, freq_num, n_bins = phi_tensor.shape
@@ -403,8 +405,9 @@ def plot_spectra_at_times(dataSet, time_indices, paramsSet, fileSet, folderSet, 
                     spectrum = phi_tensor[t_idx, :, bin_idx]
                 else:
                     spectrum = phi_tensor[t_idx, :, bin_idx]
+                    print(f"Time {t_idx} - Min: {np.nanmin(spectrum)}, Max: {np.nanmax(spectrum)}, NaNs: {np.isnan(spectrum).sum()}")
                     freq = freqBase
-                plt.plot(freq, spectrum, label=f'Time: {t_idx}, {methodName} with {file} init {params["groups"]} groups for {timeSteps} steps')
+                plt.plot(freq, spectrum, linestyle=style, label=f'Time: {t_idx}, {methodName} with {file} init {params["groups"]} groups for {timeSteps} steps')
             else:
                 print(f"Warning: Time step index {t_idx} is out of bounds (max {n_steps - 1}) and skipped.")
 